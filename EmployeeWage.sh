d46dcee5a9897b71cad0e8b8ef1a5b78d3af65ad
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-13 11:13:07 +0530

    Added :Calculate daily wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-isPresent=1;
-attendence=$(( RANDOM % 2 ));
 
-if [ $isPresent -eq $attendence ]
+attendence=$(( RANDOM % 2 ))
+if(( attendence == 0 ))
 then
-    echo "Employee is Present"
+    echo "Employee is absent"
 else
-    echo "Employee is Abscent"
+    echo "Employee is present"
 fi
+
+#calculation of daily employee wage
+wage_per_hour=20
+no_of_hours=8
+echo "The Daily Employee Wage is $(( attendence * wage_per_hour * no_of_hours ))"
