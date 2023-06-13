bfa8325102c42ee25b410704764a8bfb43e7e24e
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-13 12:12:48 +0530

    Added :Store the Daily Wage along with the Total Wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 get_work_hrs ()
-{   
-    emp_attendence=$1
-     case $emp_attendence in 
+{
+    emp_attendance=$1
+    case $emp_attendance in
         0)
             no_of_hrs_per_day=0
             ;;
@@ -21,15 +22,22 @@ max_hrs=100
 max_days=20
 total_working_hrs=0
 total_working_days=0
-while(( total_working_hrs < max_hrs && total_working_days < max_days ))
+declare -A daily_wages
+
+while (( total_working_hrs < max_hrs && total_working_days < max_days ))
 do
     (( total_working_days++ ))
-    emp_attendence=$(( RANDOM % 3 ))
-    no_of_hrs_per_day=$( get_work_hrs $emp_attendence)
+    emp_attendance=$(( RANDOM % 3 ))
+    no_of_hrs_per_day=$( get_work_hrs $emp_attendance )
     (( total_working_hrs += no_of_hrs_per_day ))
-    
+    daily_wage=$(( no_of_hrs_per_day * wage_per_hr ))
+    daily_wages["Day $total_working_days"]=$daily_wage
 done
 
 total_salary=$(( total_working_hrs * wage_per_hr ))
 echo "Total salary is $total_salary"
-
+echo "Daily wages:"
+for key in "${!daily_wages[@]}"
+do
+    echo "$key: ${daily_wages[$key]}"
+done
